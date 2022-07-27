6004e0b3e5fe38ff1a29f82b7a7e7839068a5998
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-07-27 12:11:18 -0500

    Implementación de Buffer Manager

---
+++ src/lru_replacer.cpp
@@ -0,0 +1,17 @@
+#include "lru_replacer.h"
+
+namespace bustub {
+
+LRUReplacer::LRUReplacer(size_t num_pages) {}
+
+LRUReplacer::~LRUReplacer() = default;
+
+bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+
+void LRUReplacer::Pin(frame_id_t frame_id) {}
+
+void LRUReplacer::Unpin(frame_id_t frame_id) {}
+
+size_t LRUReplacer::Size() { return 0; }
+
+}  // namespace bustub
