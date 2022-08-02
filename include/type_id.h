e1fc03e1583b120d291a2c92c34c0f8ee4f73989
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-08-01 23:07:35 -0500

    Version Anterior

--- include/type_id.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace bustub {
-// Every possible SQL type ID
-enum TypeId { INVALID = 0, BOOLEAN, TINYINT, SMALLINT, INTEGER, BIGINT, DECIMAL, VARCHAR, TIMESTAMP };
-}  // namespace bustub
